2e90604bc2de219fdf61d07506127dbc887593f5
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2025-07-16 16:14:16 -0300

    .

---
+++ langs/cuda/teste_desempenho.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+N="$1"
+
+if [ -z "$1" ]; then
+        echo "Insert the integer arg: "
+	echo "./teste_desempenho.sh 100000"
+else
+	echo "Teste de desempenho para GPU($N)"
+	nvcc vector_add_threads.cu -o gpu ; time ./gpu $N ; rm gpu
+	
+	echo "Teste de desempenho para CPU($N)"
+	gcc vector_add.c -o cpu ; time ./cpu $N ; rm cpu
+
+	echo "Teste de desempenho para OpenMP($N)"
+	gcc -o omp -fopenmp vector_add_omp.c ; time ./omp $N ; rm omp
+fi
+
